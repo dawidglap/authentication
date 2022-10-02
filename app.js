5c8d7964960106d345eab581a4aeff0cbe0b0824
Dawid Glapiak <dg18@live.it>
2022-10-02 15:52:33 +0200

    added env variables and gitignore

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
@@ -6,6 +7,7 @@ const encrypt = require("mongoose-encryption")
 const { MongoServerSelectionError } = require("mongodb")
 
 const app = express()
+
 
 app.use(express.static("public"))
 app.set('view engine', 'ejs')
@@ -18,8 +20,8 @@ const userSchema = new mongoose.Schema ({
     password: String
 })
 
-const secret = "Thisisourlittlesecret."
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] })
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] })
 
 const User = new mongoose.model("User", userSchema)
 
